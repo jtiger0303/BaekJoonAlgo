c11e114593348a069c0b1519b9fc239d0c0e257d
jtiger0303 <jtigerjh0303@gmail.com>
2022-01-03 02:29:14 +0900

    Add files via upload
    Initial Commit of BOJ2156 on dynamic programming

---
+++ BOJ2156.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+using namespace std;
+int d[10001];
+int a[10001];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    d[1] = a[1];
+    d[2] = a[1] + a[2];
+
+    for (int i = 3; i <= n; i++) {
+        d[i] = d[i - 1];
+        if (d[i] < d[i - 2] + a[i]) {
+            d[i] = d[i - 2] + a[i];
+        }
+        if (d[i] < d[i - 3] + a[i]+a[i-1]) {
+            d[i] = d[i - 3] + a[i]+a[i-1];
+        }
+    }
+    cout << d[n] << endl;
+    return 0;
+}
+

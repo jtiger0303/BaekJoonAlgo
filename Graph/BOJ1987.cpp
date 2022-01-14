d88c0eca9301186e104211dd59d6f96de9871d95
jtiger0303 <jtigerjh0303@gmail.com>
2022-01-15 01:41:00 +0900

    Initial Commit for BOJ1987 of DFS
    Using dfs(int y, int x, int cnt)

---
+++ Graph/BOJ1987.cpp
@@ -0,0 +1,54 @@
+﻿// BOJ1987.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int r, c;
+int ans = 0;
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+char board[21][21];
+int alpha[26] = { 0 };
+void dfs(int y, int x, int cnt) {
+    ans = max(ans, cnt);
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (nx < 0 || ny < 0 || nx >= c || ny >= r) continue;
+        
+        int k = (int)board[ny][nx] - 'A';
+        if (alpha[k]) continue; // 방문했었던 알파벳인 경우 패스
+        alpha[k]++;
+        dfs(ny, nx, cnt + 1);
+        alpha[k]--;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> r >> c;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> board[i][j];
+        }
+    }
+    alpha[(int)board[0][0] - 'A']++; //(0,0) 추가
+    dfs(0, 0, 1);
+    cout << ans << '\n';
+    return 0;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.

6de31d5c6935f77a5149166a240e190282b9a440
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-01 02:44:35 +0900

    Update BOJ1987 adding a new way
    using exhausting search

--- Graph/BOJ1987.cpp
+++ Graph/BOJ1987.cpp
@@ -38,4 +38,39 @@ int main()
     cout << ans << '\n';
     return 0;
 }
+----------------------------------<2.완전탐색>----------------------------------------
+#include <iostream>
+using namespace std;
+int R, C, ret, visited[30];
+char a[21][21];
+const int dy[4] = { -1, 0, 1, 0 };
+const int dx[4] = { 0, 1, 0, -1 };
+void go(int y, int x, int cnt) {
+    ret = max(ret, cnt);
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (ny < 0 || ny >= R || nx < 0 || nx >= C) continue;
+        int next = (int)(a[ny][nx] - 'A');
+        if (!visited[next]) {
+            visited[next] = 1;
+            go(ny, nx, cnt + 1);
+            visited[next] = 0;//완전탐색//
+        }
+    }
+    return;
+}
+int main()
+{
+    cin >> R >> C;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> a[i][j];
+        }
+    }
+    visited[(int)a[0][0] - 'A'] = 1;
+    go(0, 0, 1);
+    cout << ret << '\n';
+    return 0;
+}
 

70c86e58c76e3bc090cff8a4d863852863f37d72
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-03 00:11:26 +0900

    Initial Commit for BOJ7568 using pair<int, int>

---
+++ bruteforce/BOJ7568.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+pair<int, int> kh[50];
+int n;
+int main()
+{
+	int rank = 1;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> kh[i].first >> kh[i].second;
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if ((kh[i].first < kh[j].first) && kh[i].second < kh[j].second) {
+				rank++;
+			}
+		}
+		cout << rank << " ";
+		rank = 1;
+	}
+	return 0;
+}

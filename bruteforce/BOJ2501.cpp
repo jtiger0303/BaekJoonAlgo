2819ff619456d941c86ad45ed5c3a5ce5a9e1663
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-05 03:05:53 +0900

    Initial Commit for BOJ2501 of division problem
    using  bruteforce

---
+++ bruteforce/BOJ2501.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int n, k;
+vector<int> num;
+int main()
+{
+	int cnt = 0;
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		if (n % i != 0) continue;
+		else {
+			num.push_back(i);
+			cnt++;
+		}
+	}
+	if (cnt < k) cout << "0";
+	else cout << num[k - 1];
+	return 0;
+}
+

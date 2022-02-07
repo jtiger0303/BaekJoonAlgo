2e1290aa32a146fe70d32eb4ebd6d3945a14a29e
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-08 03:08:22 +0900

    Initial Commit for BOJ2702 of gcd/lcm

---
+++ bruteforce/BOJ2702.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int t, n, m;
+int gcd(int a, int b) {
+    if (a == 0) return b;
+    return gcd(b % a, a);
+}
+int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> n >> m;    
+        cout << lcm(n, m) <<" "<< gcd(n, m) << '\n';
+    }
+   
+    return 0;
+
+}
+

b2d475a909c4d496c08dfd31b18e4e748b66c3c2
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-10 01:57:04 +0900

    Initial Commit for BOJ11723 using all cases
    using bitmasking to remove, add, and etc

---
+++ Bitmask/BOJ11723.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int n, m, x;
+char s[11];
+int main()
+{
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> s >> x;
+        if (s[0] == 'a' && s[1] == 'd') n |= (1 << x);//합연산
+        else if (s[0] == 'r') n &= ~(1 << x);
+        else if (s[0] == 'c') cout << ((n & (1 << x)) == 0 ? 0 : 1);
+        else if (s[0] == 't') n ^= (1 << x);
+        else if (s[0] == 'a' && s[1] == 'l') n = (1 << 21) - 1;
+        else n = 0;
+    }
+
+}
+

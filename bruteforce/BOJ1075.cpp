b5dd354af6702737df3582361a0b81f974184f2d
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-10 02:37:35 +0900

    Initial Commit for BOJ1075 of dividing
    using bruteforce( N/=100->N*=100)

---
+++ bruteforce/BOJ1075.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int n, f;
+void turn(int num, int div) {
+    int ans = 0;
+    
+    num /= 100;
+    num *=100;
+    while ((num + ans) % div != 0 && div <= 100) ans++;
+    if (ans < 10) cout << "0" << ans;
+    else cout << ans;
+
+}
+int main()
+{
+    cin >> n >> f;
+    turn(n, f);
+    return 0;
+}
+

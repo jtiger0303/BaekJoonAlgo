62e7d00ba4f38fa60585cfb57d94b08367f76c99
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-12 01:26:25 +0900

    Initial Commit for BOJ13244 using tree definition

---
+++ Bitmask/BOJ13244.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int t, n, m, a, b, visited[1004], cnt;
+vector<int> adj[1004];
+void dfs(int here) {
+    visited[here] = 1;
+    for (int there : adj[here]) {
+        if (!visited[there]) {
+            dfs(there);
+        }
+    }
+    return;
+}
+int main()
+{
+    cin >> t;
+    while (t--) {
+        for (int i = 0; i < 1004; i++) {
+            adj[i].clear();
+        }
+        fill(visited, visited + 1004, 0);
+        cnt = 0;
+        cin >> n >> m;
+        for (int i = 0; i < m; i++) {
+            cin >> a >> b;
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                cnt++;
+            }
+        }
+        if (m == n - 1 && cnt == 1) puts("tree");
+        else puts("graph");
+    }
+}
+

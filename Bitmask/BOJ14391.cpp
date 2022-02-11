529328fd6ebee2cb6576fa824a92ef48a876c789
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-11 23:26:01 +0900

    Initial Commit for BOJ14391 of bitmasking

---
+++ Bitmask/BOJ14391.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+int a[4][4], n, m, ret;
+int main()
+{
+    scanf_s("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf_s("%1d", &a[i][j]);
+        }
+    }
+    for (int s = 0; s < (1 << (n * m)); s++) {//경우의 수//
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int cur = 0;
+            for (int j = 0; j < m; j++) {
+                int k = i * m + j;
+                if ((s & (1 << k)) == 0){//가로가 0, 세로가 1
+                    cur = cur * 10 + a[i][j];
+                }
+                else {
+                    sum += cur;
+                    cur = 0;
+                }
+            }
+            sum += cur;
+
+        }//가로만 체크
+        for (int j = 0; j < m; j++) {
+            int cur = 0;
+            for (int i = 0; i < n; i++) {
+                int k = i * m + j;
+                if ((s & (1 << k)) != 0) {
+                    cur = cur * 10 + a[i][j];
+                }
+                else {
+                    sum += cur;
+                    cur = 0;
+                }
+            }
+            sum += cur;
+        }
+        ret = max(ret, sum);
+    }
+    cout << ret << '\n';
+    return 0;
+}

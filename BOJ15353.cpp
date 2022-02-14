5fc9239fd69deff467e66b221d8c5ce115db34bc
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-15 01:22:41 +0900

    Initial Commit for BOJ15353
    using mathematics

---
+++ BOJ15353.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string string_add(string a, string b) {
+    int sum = 0;
+    string ret;
+    while (a.size() || b.size() || sum) {
+        if (a.size()) {
+            sum += a.back() - '0';
+            a.pop_back();
+        }
+        if (b.size()) {
+            sum += b.back() - '0';
+            b.pop_back();
+        }
+        ret += (sum % 10) + '0';
+        sum /= 10;
+    }
+    reverse(ret.begin(), ret.end());
+    return ret;
+}
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+    cout << string_add(a, b) << '\n';
+    return 0;
+}
+

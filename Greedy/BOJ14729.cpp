a9823cfe5d1a6cdbe18b76ddd87cb17214fd557b
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-17 01:11:37 +0900

    Initial Commit for BOJ14729 practicing pq

---
+++ Greedy/BOJ14729.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+int n;
+double temp;
+priority_queue<double> pq;
+vector<double> v;
+
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        if (pq.size() == 7) {
+            pq.push(temp);
+            pq.pop();
+        }
+        else pq.push(temp);
+    }
+    while (pq.size()) {
+        v.push_back(pq.top());
+        pq.pop();
+    }
+    sort(v.begin(), v.end());
+    for (double i : v) cout << i << '\n';
+    return 0;
+}
+

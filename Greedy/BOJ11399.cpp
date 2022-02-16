2c411aeb156d9d3e67202feea7f8dc379d0c7958
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-17 03:45:14 +0900

    Initial Commit for BOJ11399 using greedy
    sorting using fibonacci

---
+++ Greedy/BOJ11399.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, ret=0;
+int main()
+{
+    cin >> n;
+    vector<int> v(n);
+    vector<int> f(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    for (int i = 1; i < n; i++) {
+        v[i] = v[i] + v[i - 1];
+    }
+    for (int i = 0; i < n; i++) {
+        ret += v[i];
+    }
+    cout << ret << '\n';
+    
+   
+    return 0;
+}
+

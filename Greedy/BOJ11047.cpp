885910ca357e616c75ad6fd31ab85fe2545dc776
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-18 17:31:28 +0900

    Initial Commit for BOJ11047 using greedy
    counting coins

---
+++ Greedy/BOJ11047.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n, k;
+int main()
+{
+    int sum = 0;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    for (int i = n-1; i >= 0; i--) {
+        while (k - v[i] >= 0) {
+            sum++;
+            k -= v[i];
+        }
+    }
+    cout << sum << '\n';
+    return 0;
+}
+

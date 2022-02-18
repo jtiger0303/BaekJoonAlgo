f86aeb8bb0c68b17f5f0d7c6d1fa6be90b9a208c
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-19 00:20:49 +0900

    Initial Commit for BOJ5585 using greedy
    money changing problem

---
+++ Greedy/BOJ5585.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int n;
+int money[6] = { 500, 100, 50, 10, 5, 1 };
+int main()
+{
+    int sum = 0;
+    cin >> n;
+    n = 1000 - n;
+    
+    for (int i = 0; i < 6; i++) {
+        if (money[i] <= n) {
+            sum += n / money[i];
+            n = n % money[i];
+        }
+        
+    }
+    cout << sum << '\n';
+    return 0;
+}
+

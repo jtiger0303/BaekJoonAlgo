46b41e6d72782e142c00f18c41906f96cc20a7b1
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-18 23:08:35 +0900

    Initial Commit for BOJ1026 using greedy
    multiplying A's minimum element  with B's maximum element

---
+++ Greedy/BOJ1026.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int sum = 0;
+int main()
+{//A의 작은 수와 B의 큰 수를 곱하자//
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    for (int i = 0; i < n; i++) {
+        sum += a[i] * b[n - 1-i];
+    }
+    cout << sum << '\n';
+    return 0;
+}
+

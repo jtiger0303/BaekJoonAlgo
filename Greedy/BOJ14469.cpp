079a3a2fc4a0f7e47c14740412d4dc1e60952bbc
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-19 02:29:24 +0900

    Initial Commit for BOJ14469 using line-sweeping

---
+++ Greedy/BOJ14469.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int main()
+{
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].first >> a[i].second;
+    }
+    sort(a.begin(), a.end());
+    int realtime = a[0].first + a[0].second;
+    for (int i = 1; i < a.size(); i++) {
+        realtime = max(realtime, a[i].first);
+        realtime += a[i].second;
+    }
+    cout << realtime << '\n';
+    return 0;
+}
+

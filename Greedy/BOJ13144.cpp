39617fe80f1ff4adf6f89b56d975df04414fe790
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-21 02:59:31 +0900

    Initial Commit for BOJ13144 using two pointers
    using combinations

---
+++ Greedy/BOJ13144.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+long long s, e, cnt[100001], n, a[100001];
+long long ret;
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    while (e < n) {
+        if (!cnt[a[e]]) {
+            cnt[a[e]]++;
+            e++;
+        }
+        else {
+            ret += (e - s);
+            cnt[a[s]]--;
+            s++;
+        }
+    }
+    ret += (long long)(e - s) * (e - s + 1) / 2;
+    cout << ret << '\n';
+    return 0;
+}
+

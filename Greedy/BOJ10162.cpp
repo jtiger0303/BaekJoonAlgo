1028c616fe8d743ccd1931171bcc2d8cc4a8f4cb
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-20 22:49:48 +0900

    Initial Commit for BOJ10162 using greedy
    adding the count

---
+++ Greedy/BOJ10162.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int n;
+int a = 150;
+int b = 60;
+int c = 10;
+int main()
+{
+    bool check = true;
+    int countA = 0;
+    int countB = 0;
+    int countC = 0;
+
+    cin >> n;
+    while (check) {
+        if (n >= a) {
+            countA += n / a;
+            n = n % a;
+        }
+        else if (n >= b) {
+            countB += n / b;
+            n = n % b;
+        }
+        else if (n >= c) {
+            countC += n / c;
+            n = n % c;
+        }
+        else check = false;
+    }
+    if (!n) cout << countA << " " << countB << " " << countC << '\n';
+    else cout << "-1" << '\n';
+    return 0;
+}
+

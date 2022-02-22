dffd54d9db7377d6a05ad3775337e473fb082471
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-23 03:06:19 +0900

    Initial Commit for BOJ1789 using greedy
    subtracting the result when the sum is bigger than the input

---
+++ Greedy/BOJ1789.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+long long s;
+long long sum = 0;
+int num = 1;
+int result = 0;
+int main()
+{
+    cin >> s;
+    while (true) {
+        sum += num;
+        result++;
+        if (sum > s) {
+            result--;//값을 넘길 시에는 줄인다//
+            break;
+        }
+        num++;
+    }
+    cout << result << '\n';
+    return 0;
+}
+

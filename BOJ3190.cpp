98b4f91975a20203b01139a0cb748d69140fd450
jtiger0303 <jtigerjh0303@gmail.com>
2022-02-25 02:39:20 +0900

    Initial Commit for BOJ3190 using deque

---
+++ BOJ3190.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+using namespace std;
+int n, k, l, y, x, t, ret, idx, dir = 1;
+int a[104][104], visited[104][104];
+char c;
+deque<pair<int, int>> dq;
+vector<pair<int, int>> _time;
+const int dy[4] = { -1, 0, 1, 0 };
+const int dx[4] = { 0, 1, 0, -1 };
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int time=0;
+	cin >> n >> k;
+	for (int i = 0; i < k; i++) {
+		cin >> y >> x;
+		a[--y][--x] = 1;
+	}
+	cin >> l;
+	for (int i = 0; i < l; i++) {
+		cin >> t >> c;
+		if (c == 'D') _time.push_back({ t, 1 });
+		else _time.push_back({ t, 3 });
+	}
+	dq.push_back({ 0, 0 });
+	while (dq.size()) {
+		time++;
+		tie(y, x) = dq.front();
+		int ny = y + dy[dir];
+		int nx = x + dx[dir];
+		if (ny >= n || ny < 0 || nx >= n || nx < 0 || visited[ny][nx]) {
+			break;
+		}
+		if (!a[ny][nx]) {
+			//사과가 없다
+			visited[dq.back().first][dq.back().second] = 0;
+			dq.pop_back();
+		}
+		else a[ny][nx] = 0;
+		visited[ny][nx] = 1;
+		dq.push_front({ ny, nx });
+		if (time == _time[idx].first) {
+			dir = (dir + _time[idx].second) % 4;
+			idx++;
+		}
+	}
+	cout << time << '\n';
+}
+

d12abbdaea4ea4b4115cf1f687ea8a6359a207b2
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-02 02:03:57 +0900

    Initial Commit for BOJ14888 using mathematics
    using bruteforce

---
+++ bruteforce/BOJ14888.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int n;
+int a[12], b[4];
+int p, minu, mult, divi;
+int ret = -1000000001;
+int ret2 = 1000000001;
+void go(int index, int cur, int plus, int minus, int mul, int div) {
+    if (index == n - 1) {
+        ret = max(cur, ret);
+        ret2 = min(ret2, cur);
+        return;
+    }
+    if (plus) go(index + 1, cur + a[index + 1], plus - 1, minus, mul, div);
+    if (minus) go(index + 1, cur - a[index + 1], plus, minus - 1, mul, div);
+    if (mul) go(index + 1, cur * a[index + 1], plus, minus, mul - 1, div);
+    if (div) go(index + 1, cur / a[index + 1], plus, minus, mul, div - 1);
+    return;
+}
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cin >> p >> minu >> mult >> divi;
+    go(0, a[0], p, minu, mult, divi);
+    cout << ret << '\n' << ret2 << '\n';
+    return 0;
+}

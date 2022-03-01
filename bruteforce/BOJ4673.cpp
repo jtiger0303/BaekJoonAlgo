18b53c76a66bbd530ccbfbbc01e4443be2d6756b
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-02 02:45:17 +0900

    Initial Commit for BOJ4673 using self-number

---
+++ bruteforce/BOJ4673.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+bool check[10001];
+int d(int num) {
+    int temp;
+    temp = num + num / 1000 + (num / 100) % 10 + (num % 100)/10 + num % 10;
+    return temp;
+}
+void selfn() {
+    int temp;
+    check[1] = false;
+    for (int i = 0; i < 10000; i++) {
+        if (i < 10000) {
+            temp = d(i);
+            if (temp < 10000) check[temp] = true;
+        }
+    }
+}
+int main()
+{
+    selfn();
+    for (int i = 1; i < 10000; i++) {
+        if (!check[i]) {
+            cout << i << '\n';
+        }
+    }
+}
+

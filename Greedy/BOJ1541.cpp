8d86343d4022c5de6abb7e885d31a5571e536831
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-03 11:35:03 +0900

    Initial Commit for BOJ1541 using greedy/stoi

---
+++ Greedy/BOJ1541.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string str;
+string temp = "";
+int sum = 0;
+bool isMinus = false; 
+int main()
+{
+    cin >> str;
+    for (int i = 0; i <= str.size(); i++) {
+        if (str[i] == '+' || str[i] == '-' || str[i] == '\0') {
+            if (isMinus) {//+, -, 끝 수이면, temp 저장한 값을 stoi함수를 통해 sum에 저장
+                sum -= stoi(temp);
+            }
+            else {
+                sum += stoi(temp);
+            }
+            temp = "";
+            if (str[i] == '-') {
+                isMinus = true;
+            }
+        }
+        else {
+            temp += str[i];//+, -, 끝 수가 아니면 temp에 저장//
+        }
+        
+    }
+    cout << sum << '\n';
+    return 0;
+}
+

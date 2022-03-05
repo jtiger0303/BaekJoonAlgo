b4c63b4ca5a5a8eecd8e796e6ac6de873bce0b07
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-05 21:06:21 +0900

    Initial Commit for BOJ2864 using greedy
    changing 6->5=minimum
    changing 5->6=maximum

---
+++ Greedy/BOJ2864.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string a, b;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a >> b;
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] == '6') {
+            a[i] = '5';
+        }
+    }
+
+    for (int i = 0; i < b.size(); i++) {
+      if (b[i] == '6') {
+            b[i] = '5';
+        }
+    }
+    cout << stoi(a) + stoi(b) << ' ';
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] == '5') {
+            a[i] = '6';
+        }
+    }
+
+    for (int i = 0; i < b.size(); i++) {
+        if (b[i] == '5') {
+            b[i] = '6';
+        }
+    }
+    cout << stoi(a) + stoi(b) << '\n';
+    return 0;
+}
+

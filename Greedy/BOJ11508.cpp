8aaa9923b2fe29ec6328d20ef07f62c83c616c4b
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-05 23:50:51 +0900

    Initial Commit for BOJ11508 using greedy
    sorting from larger to smaller

---
+++ Greedy/BOJ11508.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int main()
+{
+	cin >> n;
+	vector<int> v(n);
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+
+	}
+	sort(v.begin(), v.end(), greater<int>());//내림차순 정렬
+	
+	for (int i = 0; i < v.size(); i++) {
+		if ((i + 1) % 3 != 0) sum += v[i];//3n번째는 할인!!//
+	}
+	cout << sum << '\n';
+	return 0;
+
+}

9f121470964f40f9404799ebec91563ab651af80
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-05 21:58:18 +0900

    Initial Commit for BOJ16953
    by calculating in reverse way

---
+++ Greedy/BOJ16953.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int a, b, cnt=0;
+int main()
+{	//거꾸로 계산하는 방식//
+	cin >> a >> b;
+
+	while (true) {
+		if (a > b) {
+			cout << "-1" << '\n';
+			break;
+		}
+		if (a == b) {
+			cnt++;
+			cout << cnt << '\n';
+			break;
+		}
+		if (b % 10 == 1) {
+			b--;
+			b /= 10;
+		}
+		else if (b % 2 == 0) {
+			b /= 2;
+		}
+		else {
+			cout << "-1" << '\n';
+			break;
+		}
+		cnt++;
+
+	}
+	
+}
+

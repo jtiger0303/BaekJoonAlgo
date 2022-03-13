b7bf4ce7326c3ddf984f1386e748181b77d163bf
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-14 03:05:24 +0900

    Initial Commit for BOJ1715 pushing top&sum into pq

---
+++ Greedy/BOJ1715.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int n, temp, sum=0;
+priority_queue<int, vector<int>, greater<int>> pq;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        pq.push(temp);
+    }
+    while (pq.size() > 1) { //작은 것부터 정렬->합친 후 다시 pq에 넣는다//
+        int a, b;
+        
+        a = pq.top();
+        pq.pop();
+
+        b = pq.top();
+        pq.pop();
+
+        sum += a + b;
+        pq.push(a + b);
+    }
+    cout << sum << '\n';
+    return 0;
+}
+

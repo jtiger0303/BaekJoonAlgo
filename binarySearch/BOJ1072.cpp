23412b43154c5269d2f9397dde8536b825baf803
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-22 02:36:36 +0900

    Initial Commit for BOJ1072 counting games

---
+++ binarySearch/BOJ1072.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+typedef long long LL;
+LL a, b, mid, ret;
+int main()
+{
+    cin >> a >> b;
+    LL z = b * 100 / a;
+    LL lo = 0, hi = 1e9;
+    while (lo <= hi) {
+        mid = (lo + hi) / 2;
+        if ((b + mid) * 100 / (a + mid) > z) {
+            ret = mid;
+            hi = mid - 1;
+        }
+        else lo = mid + 1;
+    }
+    cout << (ret ? ret : -1) << '\n';
+    return 0;
+}
+

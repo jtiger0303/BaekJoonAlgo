831f0fd4fe2c08d3b8f718637242581b1297c89a
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-27 03:04:36 +0900

    Initial Commit for BOJ9663 using NQueen

---
+++ bruteforce/BOJ9663.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int col[15], n, sum = 0;
+bool check(int level) {
+    for (int i = 0; i < level; i++) {
+        if (col[i] == col[level] || abs(col[level] - col[i]) == level - i){
+            //같은 줄에 있거나, 대각선에 위치해 있을 떄//
+            return false;
+        }
+    }
+    return true;
+}
+void nqueen(int x) {
+    if (x == n) sum++;
+    else {
+        for (int i = 0; i < n; i++) {
+            col[x] = i;//말을 배치
+            if (check(x)) {
+                nqueen(x + 1);
+            }
+        }
+    }
+}
+int main()
+{
+    cin >> n;
+    nqueen(0);
+    cout << sum << '\n';
+    return 0;
+}
+

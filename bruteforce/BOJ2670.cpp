f7c22b681a06789eeb0213c1f119af2e85fce01f
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-27 02:16:35 +0900

    Initial Commit for BOJ2670 checking the largest

---
+++ bruteforce/BOJ2670.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int n;
+double a[10001], ret, k;
+
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    double b = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] > b * a[i]) {
+            b = a[i];
+            //곱할 필요 X//
+        }
+        else b *= a[i];
+        ret = max(b, ret);
+    }
+    printf("%.3lf", ret + 0.00001);
+    return 0;
+}
+

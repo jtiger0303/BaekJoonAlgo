a929d28096af8470532c959c5d545a9397dabdc8
jtiger0303 <jtigerjh0303@gmail.com>
2022-03-29 02:34:55 +0900

    Initial Commit for BOJ15989 using bottom-up
    finding 1+2+3

---
+++ DP/BOJ15989.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+typedef long long LL;
+LL t, n, dp[10001];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> t;
+    dp[0] = 1;
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 10000; j++) {
+            if (j - i >= 0) dp[j] += dp[j - i];
+
+        }
+    }
+    while (t--) {
+        cin >> n;
+        cout << dp[n] << '\n';
+    }
+    return 0;
+}
+

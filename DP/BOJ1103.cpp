39d0fd3f9b5b4c6748d92fa7f011bba3bd40874e
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-04 03:59:17 +0900

    Initial Commit for BOJ1103 of coin game

---
+++ DP/BOJ1103.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int t, a, d[54][54];
+string s;
+char b[54][54];
+bool check[54][54];
+const int dy[4] = { -1, 0, 1, 0 };
+const int dx[4] = { 0, 1, 0, -1 };
+
+bool in(int aa, int bb) {
+    return (1 <= aa && aa <= t && 1 <= bb && bb <= a);
+}
+int down(int y, int x) {
+    if (!in(y, x) || b[y][x] == 'H') return 0;//구멍에 빠지거나 보드의 바깥//
+    if (check[y][x]) {
+        cout << -1 << '\n';
+        exit(0);
+    }
+    int& ret = d[y][x];
+    if (ret) return ret;
+    check[y][x] = 1;
+    int value = (int)b[y][x] - '0';
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i] * value;
+        int nx = x + dx[i] * value;
+        ret = max(ret, down(ny, nx) + 1);
+    }
+    check[y][x] = 0;//visited 한 곳을 다시 visited하는 경우 무한히 간다//
+    return ret;
+}
+int main()
+{
+    cin >> t >> a;
+    for (int i = 1; i <= t; i++) {
+        cin >> s;
+        for (int j = 1; j <= a; j++) {
+            b[i][j] = s[j - 1];
+        }
+    }
+    cout << down(1, 1) << '\n';
+    return 0;
+}
+

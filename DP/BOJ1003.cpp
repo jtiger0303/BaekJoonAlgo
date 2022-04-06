65aa63b57899345b34462a826694cc0a10004eec
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-06 22:51:42 +0900

    Initial Commit for BOJ1003 counting fibonaccis

---
+++ DP/BOJ1003.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int zero[41] = { 1, 0, };
+int one[41] = { 0, 1, };
+
+int main()
+{
+    int t, num;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> num;
+        for (int j = 2; j <= num; j++) {
+            zero[j] = zero[j - 1] + zero[j - 2];
+            one[j] = one[j - 1] + one[j - 2];
+        }
+        cout << zero[num] << ' ' << one[num] << '\n';
+    }
+    return 0;
+}
+

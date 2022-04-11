f2def9e3030000608e14e9696db6fdc827f14cee
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-12 03:43:17 +0900

    Initial Commit for BOJ2748 using long long fibonacci

---
+++ DP/BOJ2748.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+long long fb[91], n;
+int main()
+{
+    cin >> n;
+
+    fb[0] = 0;
+    fb[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        fb[i] = fb[i - 1] + fb[i - 2];
+    }
+    cout << fb[n] << '\n';
+    return 0;
+}
+

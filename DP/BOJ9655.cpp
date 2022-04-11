05e762594e7c48e9f38804ed1057059aae55a436
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-11 23:48:40 +0900

    Initial Commit for BOJ9655 of stone game

---
+++ DP/BOJ9655.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, dp[1001];
+int main()
+{
+    cin >> n;
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = min((dp[i - 3] + 1), (dp[i - 1] + 1));
+    }
+
+    if (dp[n] % 2 == 0) {
+        cout << "CY" << '\n';
+    }
+    else {
+        cout << "SK" << '\n';
+    }
+    return 0;
+}
+

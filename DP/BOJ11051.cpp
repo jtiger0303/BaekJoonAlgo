d4058734302db14d1ac814efaa8c8d1918275671
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-13 22:01:23 +0900

    Initial Commit for BOJ11051 calculating 이항계수

---
+++ DP/BOJ11051.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int dp[1001][1001] = { 0, };
+int n, k;
+int main() {
+	cin >> n >> k;
+
+	dp[1][1] = 1;
+	dp[1][0] = 1;
+
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j <= k; j++) {
+			if (j == 0 || i == j) dp[i][j] = 1;
+			else {
+				dp[i][j] = dp[i - 1][j]%10007 + dp[i - 1][j - 1]%10007;
+			}
+		}
+	}
+	cout << (dp[n][k] % 10007)<<'\n';
+	return 0;
+}

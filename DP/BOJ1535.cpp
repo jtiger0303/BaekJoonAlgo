564a2ff2de3790b9c6681f450416546b6823dcac
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-18 14:52:08 +0900

    Initial Commit for BOJ1535 making the maximum happiness

---
+++ DP/BOJ1535.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, dp[101], cost[101], happy[101];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> cost[i];
+    for (int i = 0; i < n; i++) cin >> happy[i];
+    for (int i = 0; i < n; i++) {
+        for (int j = 100; j > cost[i]; j--) {
+            dp[j] = max(dp[j], dp[j - cost[i]] + happy[i]);
+        }
+    }
+    cout << dp[100] << '\n'; //어떤 인덱스에서의 최대치//
+    return 0;
+}
+

7d66675063436c7046636c5589ff96a173f0151b
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-24 02:41:24 +0900

    Initial Commit for BOJ2747 using dp for fibonnacci

---
+++ implementation/BOJ2747.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+typedef long long LL;
+using namespace std;
+int num;
+LL fib[46] = {};
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> num;
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= num; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    cout << fib[num] << '\n';
+    return 0;
+}
+

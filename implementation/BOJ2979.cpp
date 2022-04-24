07061c4f0b45ea75c899ffdcc560e4139eab386d
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-25 00:49:39 +0900

    Initial Commit for BOJ2979 adding the parking fee

---
+++ implementation/BOJ2979.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int A, B, C, a, b, cnt[104], ret;
+int main()
+{
+    cin >> A >> B >> C;
+    for (int i = 0; i < 3; i++) {
+        cin >> a >> b;
+        for (int j = a; j < b; j++) cnt[j]++;
+    }
+    for (int i = 1; i < 100; i++) {
+        if (cnt[i]) {
+            if (cnt[i] == 1) ret += A;
+            else if (cnt[i] == 2) ret += 2 * B;
+            else if (cnt[i] == 3) ret += 3 * C;
+        }
+    }
+    cout << ret << '\n';
+    return 0;
+}
+

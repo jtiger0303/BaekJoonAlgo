f15b84eefb0b0e7c519d496eaad7806a1ea76f6c
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-25 23:42:39 +0900

    Initial Commit for BOJ2750 using sorting

---
+++ implementation/BOJ2750.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int main()
+{
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << '\n';
+    }
+    return 0;
+}
+

f1a8ed1067ee405c340b3507ed57544af48e4917
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-30 16:13:45 +0900

    Initial Commit for BOJ10797 counting the car

---
+++ implementation/BOJ10797.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int n, car;
+int main()
+{
+    int sum = 0;
+    cin >> n;
+    for (int i = 0; i < 5; i++) {
+        cin >> car;
+        if (n == car) sum++;
+    }
+    cout << sum << '\n';
+    return 0;
+   
+}
+

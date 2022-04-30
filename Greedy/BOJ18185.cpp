7276e0cc6769f2e5486f26af68c7cb9118ed24bb
jtiger0303 <jtigerjh0303@gmail.com>
2022-04-30 17:25:01 +0900

    Initial Commit for BOJ18185 counting the price of ramen

---
+++ Greedy/BOJ18185.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int factory[10004];
+int n;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int count, count2;
+    int answer = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> factory[i];
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (factory[i + 1] > factory[i + 2]) {
+            //3개 불가능//
+            count = min(factory[i], factory[i + 1] - factory[i + 2]);
+            answer += 5 * count;
+
+            factory[i] -= count;
+            factory[i + 1] -= count;
+
+            count2 = min(factory[i], min(factory[i + 1], factory[i + 2]));
+            answer += 7 * count2;
+
+            factory[i] -= count2;
+            factory[i + 1] -= count2;
+            factory[i + 2] -= count2;
+
+
+        }
+        else {
+            //3개 가능//
+            count = min(factory[i], min(factory[i + 1], factory[i + 2]));
+            answer += 7 * count;
+
+            factory[i] -= count;
+            factory[i+1] -= count;
+            factory[i + 2] -= count;
+
+            count2 = min(factory[i], factory[i + 1]);
+            answer += 5 * count2;
+            factory[i] -= count2;
+            factory[i + 1] -= count2;
+        }
+        answer += 3 * factory[i];
+        factory[i] = 0;
+    }
+    cout << answer << '\n';
+    return 0;
+}
+

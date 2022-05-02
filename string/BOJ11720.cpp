7f9262d747ef8a0569e1457411ee92abc1d25501
jtiger0303 <jtigerjh0303@gmail.com>
2022-05-03 01:06:48 +0900

    Initial Commit for BOJ11720 summing up the characters

---
+++ string/BOJ11720.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+char ch;
+int sum = 0;
+int n;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> ch;
+        sum += ch - '0';
+    }
+    cout << sum << '\n';
+    return 0;
+}
+

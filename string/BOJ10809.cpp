5ae0c2686542be9cda6cafaf9ce29f796558f931
jtiger0303 <jtigerjh0303@gmail.com>
2022-05-03 02:02:23 +0900

    Initial Commit for BOJ10809 finding the alphabet

---
+++ string/BOJ10809.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string str;
+int cnt[26];
+int main()
+{
+	cin >> str;
+	string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i < 26; i++) {
+		cout << (int)str.find(alphabet[i])<<' '; //int로 변환해주어야 없을 때 -1반환//
+	}
+	return 0;
+	
+
+}
+

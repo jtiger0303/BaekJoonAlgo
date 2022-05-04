a160d4670ba263bd3e0d8e3ab71f63244ae790c2
jtiger0303 <jtigerjh0303@gmail.com>
2022-05-05 02:33:03 +0900

    Initial Commit for BOJ1977 of perfect square

---
+++ bruteforce/BOJ1977.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m, sum=0;
+int cnt = 0;
+vector<int> v;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int num;
+    
+    cin >> m >> n;
+    for (int i = 1; i <= 100; i++) {
+        num = i * i;
+        if (m <= num && num <= n) {
+            v.push_back(num);
+            cnt++;
+        }
+    }
+    if (cnt == 0) {
+        cout << "-1" << '\n';
+        return 0;
+    }
+    
+    for (int a : v) {
+        sum += a;
+    }
+    cout << sum << '\n';
+    cout << v[0] << '\n';
+    
+}
+

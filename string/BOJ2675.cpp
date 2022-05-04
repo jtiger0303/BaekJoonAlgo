fb40f588ee002c3a9f2a052e22381f8ea45843d0
jtiger0303 <jtigerjh0303@gmail.com>
2022-05-05 01:04:54 +0900

    Initial Commit for BOJ2675 adding the character into string

---
+++ string/BOJ2675.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string s, ans;
+int n, r;
+int main()
+{
+    cin >> r;
+    while (r--) {
+        cin >> n>>s;
+        for (int i = 0; i < s.size(); i++) {
+            for (int j = 0; j < n; j++) {
+                ans += s[i];
+            }
+        }
+        cout << ans << '\n';
+        ans.clear();
+    }
+    
+    return 0;
+
+}
+

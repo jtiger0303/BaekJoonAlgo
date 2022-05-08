d67e08eba7a268c1aa7e718c1755fa5525ce46c7
jtiger0303 <jtigerjh0303@gmail.com>
2022-05-08 23:56:01 +0900

    Initial Commit for BOJ1427 char->int

---
+++ string/BOJ1427.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+vector<int> v;
+string str;
+int main()
+{
+    cin >> str;
+
+    for (int i = 0; i < str.size(); i++) {
+        v.push_back(str[i]-'0');
+    }
+    sort(v.begin(), v.end(), greater<int>());
+
+    for (int a : v) {
+        cout << a;
+    }
+    return 0;
+}
+

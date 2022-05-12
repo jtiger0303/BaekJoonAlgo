a3b31f2c07e6cb327f64e974bc0a15231ba33277
jtiger0303 <jtigerjh0303@gmail.com>
2022-05-12 11:48:44 +0900

    Initial Commit for BOJ2908 comparing opposite number

---
+++ string/BOJ2908.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+string a, b, res;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a >> b;
+
+    for (int i = 2; i >= 0; i--) {
+        if (a[i] == b[i]) continue;
+        if (a[i] > b[i]) {
+            res = a;
+            break;
+        }
+        else {
+            res = b;
+            break;
+        }
+    }
+
+    cout << res[2] << res[1] << res[0];
+    
+    return 0;
+
+    
+}
+

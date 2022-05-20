48ac2c22779c42099dbea0354085fac87a655666
jtiger0303 <jtigerjh0303@gmail.com>
2022-05-20 21:38:27 +0900

    Initial Commit for BOJ2577 using to_string

---
+++ implementation/BOJ2577.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int a, b, c;
+string ans;
+int cnt[10];
+int main()
+{
+    cin >> a >> b >> c;
+
+    ans = to_string(a * b * c);
+    for (int i = 0; i < ans.size(); i++) {
+        cnt[ans[i] - '0']++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << cnt[i] << '\n';
+    }
+    return 0;
+}
+

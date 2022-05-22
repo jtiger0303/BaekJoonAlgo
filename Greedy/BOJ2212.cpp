3917c4ab3f3a8ac32b8db5d422702dece5354671
jtiger0303 <jtigerjh0303@gmail.com>
2022-05-22 23:54:53 +0900

    Initial Commit for BOJ2212 of the minimum sensor

---
+++ Greedy/BOJ2212.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, k;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> k;
+    vector<int> v(n);
+    vector<int> dist(n-1);
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i <n-1; i++) {
+        dist[i] = v[i + 1] - v[i];
+    }
+    sort(dist.begin(), dist.end());
+    for (int i = 0; i < n - k; i++) {
+        sum += dist[i];
+    }
+    cout << sum << '\n';
+    return 0;
+}
+

9bbd8d9b06fa7f0bf1c23d6612f7c4ffe4cac6fb
jtiger0303 <jtigerjh0303@gmail.com>
2022-05-29 23:55:25 +0900

    Initial Commit for BOJ12904 comparing s from t
    using pop_back and reverse

---
+++ Greedy/BOJ12904.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+string s, t;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> s >> t;
+    int result = 0;
+    
+    //T에서 S를 추론해보자~~//
+    while (true) {
+        if (s.size() == t.size()) {
+            if(s==t) result = 1;
+            break;
+        }
+        if (t[t.size() - 1] == 'A') {
+            t.pop_back();
+        }
+        else {
+            t.pop_back();
+            reverse(t.begin(), t.end());
+        }
+    }
+    cout << result << '\n';
+    return 0;
+    
+}
+

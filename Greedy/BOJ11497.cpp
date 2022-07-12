fb85821e7e9c71a5258b32c5756cb7648e134ed9
jtiger0303 <jtiger0303@ajou.ac.kr>
2022-07-13 01:24:10 +0900

    Initial Commit for BOJ11497 subtracting v[i]&v[i+2]

---
+++ Greedy/BOJ11497.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int n, t;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        vector<int> v(n);
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        for (int i = 0; i < v.size()-2; i++) {
+            result = max(result, abs(v[i] - v[i + 2]));
+        }
+        cout << result<<'\n';
+    }
+    return 0;
+}

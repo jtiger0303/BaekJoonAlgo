95a9ac9966bd9584a0e13c8592ca740df1e1ac0e
jtiger0303 <jtiger0303@ajou.ac.kr>
2022-07-17 00:04:59 +0900

    Initial Commit for BOJ2914 calculating the melody

---
+++ implementation/BOJ2914.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int a, i, mel;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a >> i;
+    mel = a * (i - 1) + 1;
+    cout << mel;
+    return 0;
+}
+

d828f07c7b7a4ee0555a62d20876ef8358d7860a
jtiger0303 <jtiger0303@ajou.ac.kr>
2022-07-26 11:58:59 +0900

    Initial Commit for BOJ1543 comparing two strings

---
+++ bruteforce/BOJ1543.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string input, output;
+int cnt = 0;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    getline(cin, output);
+    getline(cin, input);
+    
+    for (int i = 0; i < output.length(); i++) {
+        bool flag=true;
+        for (int j = 0; j < input.length(); j++) {
+            if (output[i + j] != input[j]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            cnt++;
+            i += input.length() - 1;
+        }
+
+    }
+    cout << cnt;
+}
+

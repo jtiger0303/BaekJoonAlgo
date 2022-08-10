193ba182c5b6cde1c283d1672c6c12386f4c6b8c
jtiger0303 <jtiger0303@ajou.ac.kr>
2022-08-11 02:11:30 +0900

    Initial Commit for BOJ14864

---
+++ mathematics/BOJ14864.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+typedef long long LL;
+int n, m, x, y, visited[100001], v[100001], flag;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> x >> y;
+        v[x]++;
+        v[y]--;
+    }
+    for (int i = 1; i <= n; i++) {
+        v[i] += i;
+        visited[v[i]]++;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (visited[i] == 0 || visited[i] >= 2) flag = 1;
+    }
+    if (flag) cout << -1 << '\n';
+    else {
+        for (int i = 1; i <= n; i++) cout << v[i] << " ";
+    }
+
+}
+

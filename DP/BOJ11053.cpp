9a1f90fa961c580c37e97f8bb9f78e51e33260cd
jtiger0303 <jtiger0303@ajou.ac.kr>
2022-09-15 02:44:20 +0900

    Initial Commit for BOJ11053 using LIS

---
+++ DP/BOJ11053.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[1001], dp[1001];
+int n;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = 1;
+        for (int j = 1; j <= n; j++) {
+            if (arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1;
+            }
+        }
+        ans = max(ans, dp[i]);
+    }
+    cout << ans;
+    return 0;
+}
+

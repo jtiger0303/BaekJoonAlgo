4bf1a364de2ff15187daede84e6fbbae7ba1b9cd
jtiger0303 <jtiger0303@ajou.ac.kr>
2022-09-16 23:50:27 +0900

    Initial Commit for BOJ9461 using DP

---
+++ DP/BOJ9461.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+long long dp[101] = { 0,1,1,1 };
+void cal(int n) {
+    for (int i = 4; i <= n; i++) {
+        dp[i] = dp[i - 2] + dp[i - 3];
+    }
+}
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        cal(n);
+        cout << dp[n] << '\n';
+    }
+    return 0;
+}

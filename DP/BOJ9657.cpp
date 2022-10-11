c9a40e204e732437dfbb670dbd2b51de513210d1
jtiger0303 <jtiger0303@ajou.ac.kr>
2022-10-12 03:53:40 +0900

    Initial Commit for BOJ9657 using dp

---
+++ DP/BOJ9657.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[1001];
+int n;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+    
+    cin >> n;
+
+    for (int i = 4; i <= n; i++) {
+        dp[n] = min({ (dp[n - 1] + 1), (dp[n - 3] + 1), (dp[n - 4] + 1) });
+    }
+
+    if (dp[n] % 2 == 0) {
+        cout << "CY" << '\n';
+    }
+    else {
+        cout << "SK" << '\n';
+    }
+    return 0;
+}
+

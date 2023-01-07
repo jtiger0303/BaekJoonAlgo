a76628bc3feba11c7bd4853ef594f3c97a0c6935
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-01-07 18:02:11 +0900

    Initial commit for BOJ15903 using sorting

---
+++ Greedy/BOJ15903.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m;
+long long sum = 0;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    vector<long long> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    while (m--) {
+        sort(v.begin(), v.end());
+        long long min = v[0] + v[1];
+        v[0] = v[1] = min;
+    }
+    for (int i = 0; i < n; i++) {
+        sum += v[i];
+    }
+    cout << sum << '\n';
+    return 0;
+    
+}

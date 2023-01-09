d81ac5fb1427af33f4ea9336030ea20ad7835160
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-01-10 03:49:28 +0900

    Initial Commit for BOJ5567 using 1차원 bfs

---
+++ Graph/BOJ5567.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int visited[502];
+int friends[502][502];
+queue<int> q;
+int n, m, a, b, cnt;
+void bfs(int x) {
+	visited[x] = 1;
+	q.push(x);
+	while (q.size()) {
+		int here = q.front();
+		q.pop();
+		if (here != 1) { //상근이의 직접적인 친구가 아닌 경우
+			for (int i = 2; i <= n; i++) {
+				if (friends[here][i] == 1 && !visited[i]) {
+					visited[i] = visited[here]+1;
+					cnt++;
+					continue;
+				}
+			}
+		}
+		else {
+			for (int i = 1; i <= n; i++) {
+				if (friends[1][i] == 1 && !visited[i]) {
+					q.push(i);
+					visited[i] = visited[here] + 1;
+					cnt++;
+				}
+			}
+		}
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 1; i <= m; i++) {
+		cin >> a >> b;
+		friends[a][b] = friends[b][a] = 1;
+	}
+	bfs(1);
+	cout << cnt << '\n';
+	return 0;
+}
+

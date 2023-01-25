79d89fd42ae0c860771e10138379f27e5d1e3c04
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-01-26 00:02:34 +0900

    Initial Commit for BOJ2295 using binary_search stl

---
+++ binarySearch/BOJ2295.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int a[1001];
+vector<int> v;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a, a + n);
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n; j++) {
+			v.push_back(a[i] + a[j]);
+		}
+	}
+	sort(v.begin(), v.end());
+	for (int i = n - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (binary_search(v.begin(), v.end(), a[i] - a[j])) {
+				cout << a[i];
+				return 0;
+			}
+		}
+	}
+}
+

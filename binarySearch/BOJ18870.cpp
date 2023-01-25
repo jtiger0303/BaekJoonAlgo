65066f0a119c5a106abe774ef0a33249bbaba54d
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-01-25 23:04:51 +0900

    Initial Commit for BOJ18870 using v.erase+unique

---
+++ binarySearch/BOJ18870.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int arr[1000000];
+vector<int> v;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        v.push_back(arr[i]);
+    }
+
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end()); //중복 원소 제거
+
+    for (int i = 0; i < n; i++) {
+        int idx = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
+        cout << idx << ' ';
+    }
+    return 0;
+}
+

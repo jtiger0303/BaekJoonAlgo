e7ac5dffd3668fdb71d6070ebb265cecd8c4f276
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-01-29 02:11:12 +0900

    Initial Commit for BOJ2579 using dp[i][j]

--- DP/BOJ2579.cpp
+++ DP/BOJ2579.cpp
@@ -1,24 +1,33 @@
 ﻿#include <iostream>
 #include <algorithm>
 using namespace std;
-int n, dp[301];
+int n, dp[301][3];
 int stairs[301];
-
+//dp[i][j]=현재까지 j개의 계단을 연속해서 밟고 i번째 계단까지 올라섰을 때 점수합의 최댓값
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         cin >> stairs[i];
     }
-
-    dp[0] = stairs[0];
-    dp[1] = max(stairs[1], stairs[0] + stairs[1]);
-    dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2]);
+    if (n == 1) {
+        cout << stairs[1];
+        return 0;
+    }
+    dp[1][1] = stairs[1];
+    dp[1][2] = 0;
+    dp[2][1] = stairs[2];
+    dp[2][2] = stairs[1] + stairs[2];
 
     for (int i = 3; i <= n; i++) {
-        dp[i] = max(stairs[i] + dp[i - 2], stairs[i] + stairs[i - 1] + dp[i - 3]);
+        dp[i][1] = max(dp[i - 2][1], dp[i - 2][2]) + stairs[i];
+        dp[i][2] = dp[i - 1][1] + stairs[i];
     }
-    cout << dp[n] << '\n';
+    cout << max(dp[n][1], dp[n][2]);
     return 0;
 }
 

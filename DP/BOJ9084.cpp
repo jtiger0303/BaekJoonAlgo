4ca0e24978bafd539341d77e6d67f477fb06dbda
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-02-01 00:12:09 +0900

    Initial Commit for BOJ9084 counting coins

---
+++ DP/BOJ9084.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int t, n, m;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> t;
+
+    while (t--) {
+        cin >> n;
+
+        vector<int> arr(n);
+       
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        cin >> m;
+        vector<int> coin(m + 1);
+        coin[0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = arr[i]; j <= m; j++) {
+                coin[j] += coin[j - arr[i]];
+            }
+        }
+        cout << coin[m] << '\n';
+    }
+    return 0;
+}
+

815dc1d91ab86ad1eb0cc3c48a3ea6d8318eea4a
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-02-02 01:36:59 +0900

    Initial Commit for BOJ7785 using unordered_set

---
+++ Hash/BOJ7785.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <string>
+#include <algorithm>
+using namespace std;
+unordered_set<string> s;
+int n;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    while (n--) {
+        string name, log;
+        cin >> name >> log;
+        if (log == "enter") s.insert(name);
+        else s.erase(name);
+    }
+    vector<string> ans(s.begin(), s.end());
+    sort(ans.begin(), ans.end(), greater<string>());
+    for (auto x : ans) cout << x << '\n';
+
+}
+

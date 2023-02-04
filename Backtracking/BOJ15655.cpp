2dee1628955336a75b1993ae9155c3c57a7cbb22
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-02-04 23:11:06 +0900

    Initial Commit for BOJ15655 of N-M(6)

---
+++ Backtracking/BOJ15655.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m;
+int a[8];
+vector<int> v;
+void go(int cnt, int index) {
+    if (cnt == m) {
+        for (int i = 0; i < m; i++) {
+            cout << a[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = index; i < n; i++) {
+        a[cnt] = v[i];
+        go(cnt + 1, i+1);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        v.push_back(a[i]);
+    }
+    sort(v.begin(), v.end());
+    go(0, 0);
+    return 0;
+
+}
+

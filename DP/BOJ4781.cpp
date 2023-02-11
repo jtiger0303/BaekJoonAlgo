7444bfb5f3d9cab175892e774c322398aaf2f65f
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-02-12 03:46:30 +0900

    Initial Commit for BOJ4781 using sum of dp from left to right

---
+++ DP/BOJ4781.cpp
@@ -0,0 +1,28 @@
+﻿#include <cstdio>
+#include <algorithm>
+#include <map>
+using namespace std;
+int dp[10001];
+int c, p;
+int main()
+{
+    //무한->왼쪽에서 오른쪽으로 dp 누적!!
+    while (1) {
+        int n, m1, m2;
+        scanf("%d %d.%d", &n, &m1, &m2);
+        if (n == 0) break;
+        int cost = m1 * 100 + m2;
+        fill(dp, dp + 10001, 0);
+        for (int i = 0; i < n; i++) {
+            scanf("%d %d.%d", &c, &m1, &m2);
+            p = m1 * 100 + m2;
+
+            for (int j = p; j <= cost; j++) {
+                dp[j] = max(dp[j], dp[j - p] + c);
+            }
+        }
+        printf("%d\n", dp[cost]);
+    }
+    return 0;
+}
+

77676a312dd7b797e5c649d177f895fd7451b59a
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-02-13 04:23:03 +0900

    Initial Commit for BOJ12865 by using another strategy

---
+++ DP/BOJ12865_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <cstdio>
+#include <algorithm>
+using namespace std;
+int dp[100001];
+int n, k, w, v;
+int main()
+{
+    scanf("%d %d", &n, &k);
+    while (n--) {
+        scanf("%d %d", &w, &v);
+        for (int j = k; j >= w; j--) {
+            dp[j] = max(dp[j], dp[j - w] + v);
+        }
+    }
+    printf("%d\n", dp[k]);
+    return 0;
+}
+

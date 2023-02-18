dd4ae677989e3f56c33a6cba8b7332ca18a67828
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-02-18 22:18:09 +0900

    Initial Commit for BOJ13913 using prev[next]=here

---
+++ Graph/BOJ13913_1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define prev aaa
+#define next aaaa
+using namespace std;
+const int max_n = 200004;
+int n, k, here, ret, next;
+int visited[max_n], prev[max_n];
+vector<int> v;
+queue<int> q;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> k;
+    visited[n] = 1;
+    q.push(n);
+    while (q.size()) {
+        here = q.front();
+        q.pop();
+        if (here == k) {
+            ret = visited[k];
+            break;
+        }
+        for (int next : {here + 1, here - 1, here * 2}) {
+            if (next >= max_n || next < 0 || visited[next]) continue;
+            visited[next] = visited[here] + 1;
+            prev[next] = here;
+            q.push(next);
+        }
+    }
+    for (int i = k; i != n; i = prev[i]) {
+        v.push_back(i);
+    }
+    v.push_back(n);
+    cout << ret - 1 << '\n';
+    reverse(v.begin(), v.end());
+    for (int i : v) cout << i << ' ';
+    return 0;
+}
+

751fc271b716cfe9f699bc4a41b29a015f9f4416
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-02-22 03:56:22 +0900

    Initial Commit for BOJ10942 using dp[from][to]

---
+++ DP/BOJ10942.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int n, m, a[2004], dp[2004][2004], t, from, to;
+//dp[from][to]
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 1; i <= n; i++) {
+        dp[i][i] = 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (a[i] == a[i + 1]) dp[i][i + 1] = 1;
+    }
+    for (int _size = 2; _size <= n; _size++) {
+        for (int i = 1; i <= n - _size; i++) {
+            if (a[i] == a[i + _size] && dp[i + 1][i + _size - 1]) {
+                dp[i][i + _size] = 1;
+            }
+        }
+    }
+    cin >> t;
+    while (t--) {
+        cin >> from >> to;
+        cout << dp[from][to] << "\n";
+    }
+    return 0;
+}

c366908ae3b21da89715d844d1aa2e81768f4de7
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-03-18 00:21:24 +0900

    Initial Commit for BOJ1074 using recursion

---
+++ BOJ1074.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+typedef long long ll;
+int n, r, c;
+ll ans = 0;
+void func(int cur, int x, int y) {
+    if (cur == 1) return;
+    ll cnt = 0;
+    for (int a = 0; a < 2; a++) {
+        for (int b = 0; b < 2; b++) {
+            if (x + a * (cur / 2) <= r && r < x + (a + 1) * (cur / 2) && y + b * (cur / 2) <= c && c < y + (b + 1) * (cur / 2)) {
+                ans += cnt;
+                func(cur / 2, x + a * (cur / 2), y + b * (cur / 2));
+                return;
+            }
+            cnt += ((cur / 2) * (cur / 2));
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> r >> c;
+
+    int v = 1;
+    for (int i = 0; i < n; i++) {
+        v *= 2;
+    }
+    func(v, 0, 0);
+    cout << ans << '\n';
+    return 0;
+}
+

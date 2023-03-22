06485943a28c6c98ac00b3d23abeb97aa85f360d
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-03-23 02:50:29 +0900

    Initial Commit for BOJ1520 using dp+dfs

---
+++ Graph/BOJ1520.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+int visited[501][501], board[501][501];
+int m, n, ret, dp[501][501];
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
+int dfs(int y, int x) {
+    if (y == m - 1 && x == n - 1) return 1;
+    if (visited[y][x]) return dp[y][x];
+
+    visited[y][x] = 1;
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || ny >= m || nx < 0 || nx >= n) continue;
+        if (board[ny][nx] >= board[y][x]) continue;
+        dp[y][x] += dfs(ny, nx);
+    }
+    return dp[y][x];
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> m >> n;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> board[i][j];
+        }
+    }
+    cout << dfs(0, 0) << '\n';
+    return 0;
+}
+

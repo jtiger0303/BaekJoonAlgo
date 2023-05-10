e2dc5f2de736761e48cd055ef877163504574308
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-05-10 15:22:25 +0900

    Initial Commit for BOJ2941 크로아티아 알파벳

---
+++ string/BOJ2941.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+vector<string> alphas = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string w;
+    cin >> w;
+
+    for (auto alpha : alphas) {
+        while (true) {
+            int pos = w.find(alpha);
+            if (pos == string::npos) break;
+            w.replace(pos, alpha.size(), "1");
+        }
+    }
+    cout << w.size() << '\n';
+    return 0;
+}
+

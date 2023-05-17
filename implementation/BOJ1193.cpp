606b964fefe52bc5212c4640a89864d7b52f53b4
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-05-17 16:06:03 +0900

    Initial Commit for BOJ1193 분수찾기

---
+++ implementation/BOJ1193.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int n;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    int i = 1;
+
+    while (n > i) {
+        n -= i;
+        i++;
+    }
+
+    if (i % 2 == 1) {
+        cout << i+1-n << '/' << n << '\n';
+    }
+    else {
+        cout << n << '/' << i + 1 - n << '\n';
+    }
+    return 0;
+}
+

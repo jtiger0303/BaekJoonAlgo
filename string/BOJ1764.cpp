2f158e66f7889f9f1076d51e4191927d4f3dc7f3
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-06-14 00:38:41 +0900

    Initial Commit for BOJ1764 듣보잠 using map

---
+++ string/BOJ1764.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+map<string, int> mp;
+vector<string> v;
+string str;
+int n, m, cnt;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        mp[str]++;
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> str;
+        mp[str]++;
+    }
+
+    for (auto c : mp) {
+        if (c.second > 1) {
+            cnt++;
+        }
+    }
+
+    cout << cnt << '\n';
+
+    for (auto c : mp) {
+        if (c.second > 1) {
+            cout << c.first << '\n';
+        }
+    }
+    return 0;
+
+}
+

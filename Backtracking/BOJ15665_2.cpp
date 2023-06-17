84d19c8a9fa66a50025c2d618e2e241c53629de7
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-06-17 17:19:52 +0900

    Initial Commit for BOJ15665 N-M(11) using set

---
+++ Backtracking/BOJ15665_2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+using namespace std;
+int n, m, arr[8], res[8];
+set<vector<int>> ans; //중복 허용 X를 위한 set
+void dfs(int cnt) {
+    if (cnt == m) {
+        vector<int> v;
+        for (int i = 0; i < m; i++) {
+            v.push_back(res[i]);
+        }
+        ans.insert(v);
+        v.clear();
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        res[cnt] = arr[i];
+        dfs(cnt + 1);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+    dfs(0);
+
+    for (auto& b : ans) {
+        for (int i = 0; i < b.size(); i++) {
+            cout << b[i] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+

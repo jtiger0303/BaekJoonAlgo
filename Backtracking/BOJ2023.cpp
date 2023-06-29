6de05b08272f9df0605531205ee52b2619575a6f
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-06-30 03:34:00 +0900

    Initial Commit for BOJ2023 신기한 소수

---
+++ Backtracking/BOJ2023.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int n;
+bool isPrime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+void dfs(int num, int len) {
+    if (len == n) {
+        cout << num << '\n';
+        return;
+    }
+    for (int i = 1; i <= 9; i++) {
+        if (isPrime(num * 10 + i)) {
+            dfs(num * 10 + i, len + 1);
+        }
+    }
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    dfs(2, 1);
+    dfs(3, 1);
+    dfs(5, 1);
+    dfs(7, 1);
+}
+

d44212c77335373099fa6ff2bfc350bc97634cb8
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-08-04 23:01:00 +0900

    Initial Commit for BOJ16918 에너지 모으기

---
+++ Backtracking/BOJ16198.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int ret = 0;
+void func(vector<int>& v, int cnt) {
+    if (v.size() == 3) {
+        cnt += v[0] * v[2];
+        ret = max(ret, cnt);
+        return;
+    }
+    for (int i = 0; i < v.size() - 2; i++) {
+        vector<int> tmp=v;
+        tmp.erase(tmp.begin() + i + 1);
+        func(tmp, cnt + (v[i] * v[i + 2]));
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    func(v, 0);
+    cout << ret << '\n';
+    return 0;
+}
+

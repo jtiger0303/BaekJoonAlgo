3c027688237250339379f577623c508d8eb5959b
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-08-29 00:45:20 +0900

    Initial Commit for BOJ1743 음식물 피하기

---
+++ Graph/BOJ1743.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
+int n, m, k, visited[101][101], board[101][101], y, x;
+vector<int> v;
+int dfs(int y, int x) {
+    int ret = 1;
+    visited[y][x] = 1;
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx] || board[ny][nx] !=1) continue;
+        ret += dfs(ny, nx);
+    }
+    return ret;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m >> k;
+
+    for (int i = 0; i < k; i++) {
+        cin >> x >> y;
+        board[x-1][y-1] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 1 && !visited[i][j]) {
+                v.push_back(dfs(i, j));
+            }
+        }
+    }
+    sort(v.begin(), v.end());
+    cout << v[v.size() - 1] << '\n';
+    return 0;
+}
+

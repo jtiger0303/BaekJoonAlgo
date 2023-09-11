7324b715e9dda5c1eaf1be4f9f0e4fc0f9d95712
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-09-11 22:21:35 +0900

    Initial Commit for BOJ16922 로마 숫자 만들기

---
+++ Backtracking/BOJ16922.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+int n, num;
+set<int> s;
+int ret[4] = { 1, 5, 10, 50 };
+void func(int begin, int cnt, int sum) {
+    if (begin >= 4) return;
+    if (cnt == n) {
+        if (s.find(sum) == s.end()) {
+            s.insert(sum);
+            num++;
+        }
+        return;
+    }
+    func(begin+1, cnt + 1, sum + ret[begin]);
+    func(begin, cnt + 1, sum + ret[begin]);
+    func(begin+1, cnt, sum);
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    func(0, 0, 0);
+    cout << num << '\n';
+    return 0;
+}
+

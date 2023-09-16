36a5cee36bec3a746f8c9e7fc0bfd1c2ffee5c22
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-09-17 00:27:37 +0900

    Initial Commit for BOJ11060 점프 점프

---
+++ DP/BOJ11060.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int dp[1001], val[1001], n;
+int main()
+{
+    cin >> n;
+    
+    //dp[i]=i번째 칸까지 올 수 있는 최소 점프 수
+    for (int i = 1; i <= n; i++) {
+        cin >> val[i];
+        dp[i] = 987654321;
+    }
+    dp[1] = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= val[i]; j++) {
+            if ((i + j) <= n) {
+                dp[i + j] = min(dp[i] + 1, dp[i + j]);
+            }
+        }
+    }
+
+    if (dp[n] == 987654321) {
+        cout << -1 << '\n';
+    }
+    else {
+        cout << dp[n] << '\n';
+    }
+    return 0;
+}
+

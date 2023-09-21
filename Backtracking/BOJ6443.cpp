49707c1797263dd2c937d0fde2ecca9656caeb8a
jtiger0303 <jtiger0303@ajou.ac.kr>
2023-09-21 21:30:57 +0900

    Initial Commit for BOJ6443 애너그램

---
+++ Backtracking/BOJ6443.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int t;
+string str;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> t;
+	while (t--) {
+		cin >> str;
+		sort(str.begin(), str.end());
+
+		do {
+			cout << str << '\n';
+		} while (next_permutation(str.begin(), str.end()));
+	}
+	return 0;
+}
+

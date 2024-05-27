b9e8cc9a756138f3e16883f1084210a766706f15
jtiger0303 <jtiger0303@ajou.ac.kr>
2024-05-27 22:32:28 +0900

    Initial Commit for 피보나치 수 5

---
+++ mathematics/BOJ10870.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int n;
+int fibo(int n) {
+    if (n == 0 || n == 1) return n;
+    return fibo(n - 1) + fibo(n - 2);
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    cout << fibo(n) << '\n';
+    return 0;
+}

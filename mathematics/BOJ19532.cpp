e6d6a6234d62b240ff9e3a6e6b631670652296cd
jtiger0303 <jtiger0303@ajou.ac.kr>
2024-05-29 11:41:07 +0900

    Initial Commit for BOJ19532 수학은 비대면강의입니다.

---
+++ mathematics/BOJ19532.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int a, b, c, d, e, f;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> a >> b >> c >> d >> e >> f;
+
+    int x = (c * e - b * f) / (a * e - b * d);
+    int y = (a * f - c * d) / (a * e - b * d);
+
+    cout << x << ' ' << y;
+    return 0;
+}
+

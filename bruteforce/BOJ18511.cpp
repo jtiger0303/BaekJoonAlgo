982a06713b4d50db3011999461eb9b6468ff79f6
jtiger0303 <jtiger0303@ajou.ac.kr>
2024-05-30 10:24:20 +0900

    Initial Commit for BOJ18511 큰 수 구성하기

---
+++ bruteforce/BOJ18511.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> v;
+int n, k, ans;
+void pick(int num) {
+    if (n < num) {
+        return;
+    }
+
+    ans = max(ans, num);
+
+    num *= 10;
+
+    for (int i = 0; i < k; i++) {
+        pick(num + v[i]);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> k;
+    v = vector<int>(k);
+
+    for (int i = 0; i < k; i++) {
+        cin >> v[i];
+    }
+
+    pick(0);
+    cout << ans << '\n';
+    return 0;
+}
+

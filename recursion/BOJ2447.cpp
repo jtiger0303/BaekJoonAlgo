b5e0f2ef926f6bf4d7fae40b6e830f4c84bf7d38
jtiger0303 <jtiger0303@ajou.ac.kr>
2024-08-10 23:05:06 +0900

    Initial Commit For BOJ2447 별 찍기 10

---
+++ recursion/BOJ2447.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int n;
+void star(int y, int x, int n) {
+    if (n == 1) {
+        cout << "*";
+        return;
+    }
+    else if ((y / (n / 3)) % 3 == 1 && (x / (n / 3)) % 3 == 1) {
+        cout << " ";
+    }
+    else {
+        star(y, x, n / 3);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            star(i, j, n);
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+

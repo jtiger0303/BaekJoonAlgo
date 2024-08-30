2bf81ac133992060c42269cd9817c83ace749b63
jtiger0303 <jtiger0303@ajou.ac.kr>
2024-08-30 17:07:33 +0900

    Initial Commit For BOJ1744 수 묶기

--- Greedy/BOJ1744.cpp
+++ Greedy/BOJ1744.cpp
@@ -1,9 +1,9 @@
 ﻿#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
-vector<int> positive, negative;
-int n, num;
+int num, sum, zcnt, n;
+priority_queue<int> ppq; // 내림차순 (1보다 클 때)
+priority_queue<int, vector<int>, greater<int>> npq; // 오름차순(음수)
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -12,29 +12,44 @@ int main()
 
     cin >> n;
 
-    int ans = 0;
     for (int i = 0; i < n; i++) {
         cin >> num;
-        if (num > 0) positive.push_back(num);
-        else negative.push_back(num);
+
+        if (num > 0) {
+            ppq.push(num);
+        }
+        else if (num == 0) {
+            zcnt++;
+        }
+        else npq.push(num);
     }
 
-    sort(positive.begin(), positive.end(), greater<int>()); //양수는 내림차순 정렬
-    sort(negative.begin(), negative.end()); //음수는 내림차순
-
-    if (positive.size() % 2 != 0) ans += positive[positive.size() - 1]; //양수가 홀수 개일 경우에는 가장 마지막 수+
-    for (int i = 0; i < positive.size() - 1; i+=2) {
-        //양수의 경우
-        if (positive[i + 1] == 1) ans += positive[i] + positive[i + 1];
-        else if(positive[i+1]>0) ans += positive[i] * positive[i + 1];
+    while (ppq.size()) {
+        int cur = ppq.top();
+        ppq.pop();
+        
+        if (cur > 1 && ppq.size() && ppq.top() > 1) {
+            sum += cur * ppq.top();
+            ppq.pop();
+        }
+        else {
+            sum += cur;
+        }
     }
 
-    if (negative.size() % 2 != 0) ans += negative[negative.size() - 1];
-    for (int i = 0; i < negative.size() - 1; i += 2) {
-        //음수의 경우
-        ans += negative[i] * negative[i + 1];
+    while (npq.size()) {
+        int cur = npq.top();
+        npq.pop();
+
+        if (npq.size()) {
+            sum += cur * npq.top();
+            npq.pop();
+        }
+        else if (!zcnt) {
+            // 0이 없다면 음수 중 절댓값이 가장 작은 것을 더함
+            sum += cur;
+        }
     }
-    cout << ans << '\n';
+    cout << sum << '\n';
     return 0;
 }
-

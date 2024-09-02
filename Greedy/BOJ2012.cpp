1e34c03b64106c191d6e646edcb70dbb137c407d
jtiger0303 <jtiger0303@ajou.ac.kr>
2024-09-02 10:51:11 +0900

    Initial Commit For BOJ2012 등수 매기기

---
+++ Greedy/BOJ2012.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int n;
+long long sum;
+vector<int> v(500005);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    v.resize(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+    
+    for (int i = 0; i < n; i++) {
+        sum += abs(v[i] - (i + 1));
+    }
+    cout << sum << '\n';
+    return 0;
+}

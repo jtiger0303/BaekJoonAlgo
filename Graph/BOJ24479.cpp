ebeb8ec5cc748320187e1605ebb1c68649c5a198
jtiger0303 <jtiger0303@ajou.ac.kr>
2024-09-03 11:39:55 +0900

    Initial Commit For BOJ24479 알고리즘 수업 - 깊이 우선 탐색 1

---
+++ Graph/BOJ24479.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m, r, visited[100005], cnt;
+vector<int> v[100005];
+void dfs(int here) {
+    cnt++;
+    visited[here] = cnt;
+
+    for (int i = 0; i < v[here].size(); i++) {
+        int there = v[here][i];
+        if (visited[there]) continue;
+        //cnt++;
+        //visited[there] = cnt;
+        dfs(there);
+    }
+    return;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m >> r;
+
+    for (int i = 1; i <= m; i++) {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        sort(v[i].begin(), v[i].end());
+    }
+    dfs(r);
+
+    for (int i = 1; i <= n; i++) {
+        cout << visited[i] << '\n';
+    }
+    return 0;
+}

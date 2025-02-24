14ff876c2456895a5892f8b7797d7e68fd114ddb
jtiger0303 <jtiger0303@ajou.ac.kr>
2025-02-24 10:29:40 +0900

    Initial Commit For BOJ1253 using two pointers

---
+++ binarySearch/BOJ1253.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int n;
+long long arr[2005];
+int ret = 0;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    if (n <= 2) {
+        cout << 0;
+        return 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int s = 0;
+        int e = n - 1;
+        while (s < e) {
+            int sum = arr[s] + arr[e];
+
+            if (sum < arr[i]) {
+                s++;
+            }
+            else if (sum > arr[i]) {
+                e--;
+            }
+            else {
+                if (s == i) s++;
+                else if (e == i) e--;
+                else {
+                    ret++;
+                    break;
+                }
+            }
+        }
+    }
+
+    cout << ret << '\n';
+    return 0;
+}

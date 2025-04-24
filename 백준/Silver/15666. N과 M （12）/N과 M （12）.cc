23dbacd495d89ec9f86e7e65757de7c885e3622e
jtiger0303 <jtiger0303@ajou.ac.kr>
2025-04-24 15:15:16 +0900

    [Silver II] Title: N과 M (12), Time: 4 ms, Memory: 2424 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -4,19 +4,19 @@
 #include <algorithm>
 using namespace std;
 int n, m, arr[10], res[10];
-set<vector<int>> ans;
-void dfs(int idx, int cnt) {
+set<vector<int>> s;
+void dfs(int indx, int cnt) {
     if (cnt == m) {
         vector<int> v;
         for (int i = 0; i < m; i++) {
             v.push_back(res[i]);
         }
-        ans.insert(v);
+        s.insert(v);
         v.clear();
         return;
     }
 
-    for (int i = idx; i < n; i++) {
+    for (int i = indx; i < n; i++) {
         res[cnt] = arr[i];
         dfs(i, cnt + 1);
     }
@@ -35,7 +35,7 @@ int main()
     sort(arr, arr + n);
     dfs(0, 0);
 
-    for (auto& b : ans) {
+    for (auto& b : s) {
         for (int i = 0; i < b.size(); i++) {
             cout << b[i] << ' ';
         }
@@ -43,3 +43,4 @@ int main()
     }
     return 0;
 }
+
